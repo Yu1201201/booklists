41c800a19144fbaa4b26eb5ae2521b051a9ab0ca
@Yu1201201 <1201201yu@gmail.com>
2019-09-14 03:56:00 +0000

    image

--- app/models/booklist.rb
+++ app/models/booklist.rb
@@ -2,7 +2,7 @@ class Booklist < ApplicationRecord
   belongs_to :user
   
   validates :title, presence: true, length: { maximum: 30 }
-  validates :review, presence: true, length: { maximum: 300 }
+  validates :review, presence: true, length: { maximum: 600 }
 
   has_many :favorites, foreign_key: 'booklist_id', dependent: :destroy
   has_many :users, through: :favorites
